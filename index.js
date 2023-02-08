f8a18edf96e2f11f85759641fd5f6a7576393a85
Kaj <tassi100@mail.chapman.edu>
2023-02-07 17:37:03 -0800

    db.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middeleware
 app.use(cors());
 //allows us to have access to request.body
 app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
