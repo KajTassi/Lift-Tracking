1efac5700107e5ca1fc41c2b4a92b88c28926a86
Kaj <tassi100@mail.chapman.edu>
2023-02-17 20:03:27 -0800

    last changes

--- server/index.js
+++ server/index.js
@@ -74,14 +74,15 @@ app.put("/todos/:id", async (req, res) => {
 
 app.delete("/todos/:id", async (req, res) => {
     try {
-        const { id } = req.params;
-        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",
-        [id]
-        );
+      const { id } = req.params;
+      const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+        id
+      ]);
+      res.json("Todo was deleted!");
     } catch (err) {
-        console.log(err.message)
+      console.log(err.message);
     }
-});
+  });
 
 
 //listening to confirm server is on
